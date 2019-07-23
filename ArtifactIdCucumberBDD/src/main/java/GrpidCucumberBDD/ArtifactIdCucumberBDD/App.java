578f19e890456aa00226efc28fbd3eb735c05af1
chandan kumar <chandan.kumar@netsutra.com>
2019-07-23 15:15:23 +0530

    Cucumber Project

---
+++ ArtifactIdCucumberBDD/src/main/java/GrpidCucumberBDD/ArtifactIdCucumberBDD/App.java
@@ -0,0 +1,13 @@
+package GrpidCucumberBDD.ArtifactIdCucumberBDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
