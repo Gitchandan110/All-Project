652006a00b51287f3ad3ae30e2b8e16c37f604be
chandan kumar <chandan.kumar@netsutra.com>
2019-07-27 18:20:36 +0530

    we

--- ArtifactMaven/src/test/java/com/TestNGScript/file/SafetyKuvrrOpenIncident.java
+++ ArtifactMaven/src/test/java/com/TestNGScript/file/SafetyKuvrrOpenIncident.java
@@ -7,43 +7,49 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.BussinessFlow.file.LoginSafetyKuvrrBL;
-import com.BussinessFlow.file.SafetyKuvrrOpenIncidentBL;
+import com.BussinessFlow.file.SafetyKuvrrIncidentBL;
 import com.Commonutills.file.Base;
 import com.Commonutills.file.ExcelUtils;
 
 public class SafetyKuvrrOpenIncident extends Base {
-	
-	SafetyKuvrrOpenIncidentBL openIncidentbl=PageFactory.initElements(driver, SafetyKuvrrOpenIncidentBL.class);
-	LoginSafetyKuvrrBL loginSafetyKuvrrBL=PageFactory.initElements(driver, LoginSafetyKuvrrBL.class);
-	
+
+	SafetyKuvrrIncidentBL incidentbl = PageFactory.initElements(driver, SafetyKuvrrIncidentBL.class);
+	LoginSafetyKuvrrBL loginSafetyKuvrrBL = PageFactory.initElements(driver, LoginSafetyKuvrrBL.class);
+
 	String LoginDataSheet = "Login";
-	
-	
+
 	@BeforeMethod
-	
+
 	public void launchSKApp() throws InterruptedException, IOException {
-		
+
 		opendriver();
-		String url=ExcelUtils.ReadExcel(LoginDataSheet, 1, 0);
+		String url = ExcelUtils.ReadExcel(LoginDataSheet, 1, 0);
 		driver.get(url);
 		Thread.sleep(5000);
-		
-			
+
 	}
-	
-	  @Test(priority=1)
+
+	@Test()
+
+	public void launchSafetyApp() throws IOException, InterruptedException {
+
+		loginSafetyKuvrrBL.fillEmail();
+		loginSafetyKuvrrBL.fillPassword();
+		loginSafetyKuvrrBL.clickContinue();
+		incidentbl.clickOpenIncident();
 		
-		public void launchSafetyApp() throws IOException, InterruptedException {
-			
-			loginSafetyKuvrrBL.fillEmail();
-			loginSafetyKuvrrBL.fillPassword();
-			loginSafetyKuvrrBL.clickContinue();
-			openIncidentbl.verifyOpenIncident();
-   
-}
-	  
+	}
+
+	@AfterMethod
+
+	public void closeBrowser() {
+
+		driver.quit();
+	}
+
 }
